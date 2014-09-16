5366922456ad100b63ed8796176a1e140c0e822a
Richard C Gerkin <rgerkin@asu.edu>
2014-09-16 13:48:05 -0700

    Extraction of UPSIT scores from bbdp data

--- __main__.py
+++ __main__.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 import upsit
 import pdbp
 import bbdp
@@ -5,8 +7,12 @@ import bbdp
 if __name__ == '__main__':
     """PDBP data"""
     pdbp_data = pdbp.load()
-    pdbp.plot_cumul_hist(pdbp_data,booklet=1)
+    upsit_key,bbdp_data = bbdp.load()
+    #pprint(upsit_key)
+    pprint(bbdp_data)
+    '''pdbp.plot_cumul_hist(pdbp_data,booklet=1)
     pdbp.plot_cumul_hist(pdbp_data,booklet=2)
     pdbp.plot_cumul_hist(pdbp_data,booklet=3)
     pdbp.plot_cumul_hist(pdbp_data,booklet=4)
     upsit.plt.show()
+    '''
--- bbdp/__init__.py
+++ bbdp/__init__.py
@@ -9,38 +9,46 @@ def load():
     module_path = os.path.dirname(os.path.realpath(__file__))
     data_path = os.path.join(module_path,'data')
     upsit_path = os.path.join(data_path,'GerkinSmithUPSITautopsy9_10_14.xlsx')
-    cp_path = os.path.join(data_path,'Clinicopathological Correlations.xlsx')
+    cp_path = os.path.join(data_path,'Clinicopathological Correlations.xls')
 
-    upsit_wb = xlrd.open_workbook(uspit_path)
+    upsit_wb = xlrd.open_workbook(upsit_path)
     cp_wb = xlrd.open_workbook(cp_path)
     
-    upsit_tests = upsit_wb.sheets[0] # allPDtests
-    upsit_smelltestkey = upsit_wb.sheets[2] # smelltestkey
+    upsit_tests = upsit_wb.sheet_by_name('allPDtests')
+    upsit_smelltestkey = upsit_wb.sheet_by_name('smellTestKey')
     
-    cp = cp_wb.sheets[0] # Only one sheet.  
+    cp = cp_wb.sheets()[0] # Only one sheet.  
 
     data = {}
     fields = {}
+    gender = {1:'M',2:'F'}
 
-    for row_num in range(upsit_tests.nrows):
-        row = upsit_purepdonly.row_values()
-        case_id = row[0]
-        data[case_id] = {
-          'expired_age':row[2],
-          'gender':row[3],
-        }
+    upsit_key = []
+    for q in range(1,41):
+        row = upsit_smelltestkey.row_values(q)
+        options = row[1:5] # 4 possible options
+        answer_num = int(row[6]-1) # Change from 0-indexed to 1-indexed.  
+        answer = options[answer_num-1]
+        upsit_key += [(options,answer_num)]
 
-        try:
-            data[int(row[0])] = {'recruitment':fields[row[1]],
-                                 'imaging':fields[row[2]]}
-        except:
-            pass
-    for row in results_reader:
-        if row[10] == '':
-            data[int(row[2])]['upsit'] = [int(row[6]),
-                                          int(row[7]),
-                                          int(row[8]),
-                                          int(row[9])]
+    headers = upsit_tests.row_values(0)
+    hd = {key:i for i,key in enumerate(headers)}
+    for row_num in range(1,upsit_tests.nrows):
+        row = upsit_tests.row_values(row_num)
+        case_id = row[hd['CaseID']]
+        if case_id not in data:
+            data[case_id] = {
+                # Age is reported as an integer.  
+                'expired_age':int(row[hd['expired_age']]), 
+                # Gender is reported as 1 or 2.  
+                'gender':gender[row[hd['tbl_donors.gender']]], 
+                'upsit':[]
+                }
+        # 40 questions. 
+        responses = [row[hd['smell_%d' % q]] for q in range(1,41)] 
+        # Change to 0-indexed.  
+        responses = [int(x)-1 if type(x) is float else None for x in responses]
+        data[case_id]['upsit'].append(responses)
 
-    data = {key:value for key,value in data.items() if 'upsit' in value}
-    return data
+    #data = {key:value for key,value in data.items() if 'upsit' in value}
+    return upsit_key,data
