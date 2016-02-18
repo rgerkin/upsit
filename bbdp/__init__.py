690f18d07027ce2ca9bcc6ae51de113aaaf33198
Richard C Gerkin <rgerkin@asu.edu>
2016-02-18 15:32:07 -0700

    Updated all notebooks and code

--- bbdp/__init__.py
+++ bbdp/__init__.py
@@ -13,129 +13,187 @@ for i in range(3):
 
 from upsit import Subject,Question,Response,QuestionSet,ResponseSet,Test,plt
 
-def load():
-    """Load Banner Brain and Body Donation Project data."""  
+def load(kind):
+	"""Load Banner Brain and Body Donation Project data."""  
 
-    module_path = os.path.dirname(os.path.realpath(__file__))
-    data_path = os.path.join(module_path,'data')
-    disease_path = os.path.join(data_path,'GerkinSmithUPSITautopsy9_10_14.xlsx')
-    ctrl_path = os.path.join(data_path,'GerkinSmithQueryControls9_17_14.xlsx')
-    cp_path = os.path.join(data_path,'Clinicopathological Correlations.xls')
+	module_path = os.path.dirname(os.path.realpath(__file__))
+	data_path = os.path.join(module_path,'data')
+	questions = []
+	test_path = os.path.join(data_path,'GerkinSmithUPSITautopsy9_10_14.xlsx')
+	test_wb = xlrd.open_workbook(test_path)
+	test_key = test_wb.sheet_by_name('smellTestKey')
+	  
+	for q in range(1,41):
+		row = test_key.row_values(q)
+		options = row[1:5] # 4 possible options
+		answer_num = int(row[6]-1) # Change from 0-indexed to 1-indexed.  
+		questions.append(Question(options,answer_num))
+	question_set = QuestionSet(questions)
 
-    disease_wb = xlrd.open_workbook(disease_path)
-    ctrl_wb = xlrd.open_workbook(ctrl_path)
-    cp_wb = xlrd.open_workbook(cp_path)
-    
-    test_key = disease_wb.sheet_by_name('smellTestKey')
-    questions = []
-    for q in range(1,41):
-        row = test_key.row_values(q)
-        options = row[1:5] # 4 possible options
-        answer_num = int(row[6]-1) # Change from 0-indexed to 1-indexed.  
-        questions.append(Question(options,answer_num))
-    question_set = QuestionSet(questions)
+	if kind == 'dugger':
+		disease_path = os.path.join(data_path,
+									'GerkinSmithUPSITautopsy9_10_14.xlsx')
+		ctrl_path = os.path.join(data_path,
+								 'GerkinSmithQueryControls9_17_14.xlsx')
+		cp_path = os.path.join(data_path,'Clinicopathological Correlations.xls')
 
-    pd_sheet = disease_wb.sheet_by_name('"pure"PDonly1test')
-    pd_subjects,pd_tests = parse_tests(pd_sheet,question_set,'pd')
+		disease_wb = xlrd.open_workbook(disease_path)
+		ctrl_wb = xlrd.open_workbook(ctrl_path)
+		cp_wb = xlrd.open_workbook(cp_path)
+	
+		pd_sheet = disease_wb.sheet_by_name('"pure"PDonly1test')
+		pd_subjects,pd_tests = parse_tests_dugger(kind,pd_sheet,question_set,
+										 subject_label='pd')
 
-    ctrl_sheet = ctrl_wb.sheet_by_name('AllNPcontrolVisits')
-    ctrl_subjects,ctrl_tests = parse_tests(ctrl_sheet,question_set,'ctrl')  
+		ctrl_sheet = ctrl_wb.sheet_by_name('AllNPcontrolVisits')
+		ctrl_subjects,ctrl_tests = parse_tests_dugger(kind,ctrl_sheet,
+											 question_set,subject_label='ctrl')  
 
-    # Currently not used.  
-    cp = cp_wb.sheets()[0] # Only one sheet.      
-    
-    subjects = ctrl_subjects.copy()
-    subjects.update(pd_subjects)
-    tests = pd_tests + ctrl_tests
-    return subjects,tests
+		# Currently not used.  
+		cp = cp_wb.sheets()[0] # Only one sheet.      
+	  
+		subjects = ctrl_subjects.copy()
+		subjects.update(pd_subjects)
+		tests = pd_tests + ctrl_tests
+	  
+	elif kind == 'hentz':
+		all_path = os.path.join(data_path,'D20Mar2015a.xls')
+		all_wb = xlrd.open_workbook(all_path)
+		all_sheet = all_wb.sheet_by_name('Data')
 
-def parse_tests(tests_sheet,question_set,subject_label=None):
-  """Parse a worksheet of tests to return subject and tests.""" 
+		subjects,tests = parse_tests_hentz(kind,all_sheet,question_set) 
+	
+	return subjects,tests
+	
+def parse_tests_hentz(kind,tests_sheet,question_set,subject_label=None):
+	"""Parse a worksheet of tests to return subject and tests.""" 
 
-  subjects = {}
-  tests = []
-  gender = {1:'M',2:'F'}  
+	subjects = {}
+	tests = []
+	
+	headers = tests_sheet.row_values(0)
+	hd = {key:i for i,key in enumerate(headers)}
+	for row_num in range(1,tests_sheet.nrows):
+		row = tests_sheet.row_values(row_num)
 
-  headers = tests_sheet.row_values(0)
-  hd = {key:i for i,key in enumerate(headers)}
-  for row_num in range(1,tests_sheet.nrows):
-      row = tests_sheet.row_values(row_num)
-      
-      case_id = row[hd['CaseID']]
-      if case_id not in subjects:
-          subject = Subject(case_id)
-          # Age is reported as an integer or as 100+.   
-          expired_age = row[hd['expired_age']]
-          subject.expired_age = 100 if expired_age=='100+' else int(expired_age)
-          # Gender is reported as 1 or 2.  
-          subject.gender = row[hd['tbl_donors.gender']]
-          if subject_label is not None:
-            subject.label = subject_label
-          subject.demented = row[hd['dementia_nos']] in [1,'yes']
-          subjects[case_id] = subject
-          
-      test_date = row[hd['smell_test_date']]
-      test_date = datetime(1900,1,1) + timedelta(int(test_date)-2)
+		case_id = row[hd['shri_case_num']]
+		if case_id not in subjects:
+			subject = Subject(case_id)
+			# Age is reported as an integer or as 100+.   
+			expired_age = row[hd['deathage']]
+			subject.expired_age = 100 if expired_age=='100+' else int(expired_age)
+			# Gender is reported as 1 or 2.  
+			subject.gender = row[hd['female']]
+			if subject_label is not None:
+				subject.label = subject_label
+			subject.dementia = 2 in row[16:25]
+			subject.stint = float(row[hd['stint']])
+			subject.other = [int(_)>0 for _ in row[5:17]+row[19:25]]
+			subject.label = 'ctrl' if int(row[hd['controlp']]) else 'other'
+			subjects[case_id] = subject
+		  
+		responses = {}
+		for q in range(1,41):
+			choice_num = row[hd['smell_%d' % q]] 
+			if type(choice_num) is float:
+				choice_num = int(choice_num)-1 # Change to 0-indexed.  
+			else:
+				choice_num = None
+			responses[q] = Response(question_set.questions[q],choice_num)
+	  
+		response_set = ResponseSet(responses,indices=responses.keys())
+		test = Test(subjects[case_id],response_set,None)
+		tests.append(test)    
 
-      responses = {}
-      for q in range(1,41):
-          choice_num = row[hd['smell_%d' % q]] 
-          if type(choice_num) is float:
-              choice_num = int(choice_num)-1 # Change to 0-indexed.  
-          else:
-              choice_num = None
-          responses[q] = Response(question_set.questions[q],choice_num)
-      
-      response_set = ResponseSet(responses,indices=responses.keys())
-      test = Test(subjects[case_id],response_set,test_date)
-      tests.append(test)    
+	return subjects,tests
 
-  return subjects,tests
+
+def parse_tests_dugger(kind,tests_sheet,question_set,subject_label=None):
+	"""Parse a worksheet of tests to return subject and tests.""" 
+
+	subjects = {}
+	tests = []
+	
+	headers = tests_sheet.row_values(0)
+	hd = {key:i for i,key in enumerate(headers)}
+	for row_num in range(1,tests_sheet.nrows):
+		row = tests_sheet.row_values(row_num)
+
+		case_id = row[hd['CaseID']]
+		if case_id not in subjects:
+			subject = Subject(case_id)
+			# Age is reported as an integer or as 100+.   
+			expired_age = row[hd['expired_age']]
+			subject.expired_age = 100 if expired_age=='100+' else int(expired_age)
+			# Gender is reported as 1 or 2.  
+			subject.gender = row[hd['tbl_donors.gender']]-1
+			if subject_label is not None:
+				subject.label = subject_label
+			subject.demented = row[hd['dementia_nos']] in [1,'yes']
+			subjects[case_id] = subject
+		  
+		test_date = row[hd['smell_test_date']]
+		test_date = datetime(1900,1,1) + timedelta(int(test_date)-2)
+
+		responses = {}
+		for q in range(1,41):
+			choice_num = row[hd['smell_%d' % q]] 
+			if type(choice_num) is float:
+				choice_num = int(choice_num)-1 # Change to 0-indexed.  
+			else:
+				choice_num = None
+			responses[q] = Response(question_set.questions[q],choice_num)
+		
+		response_set = ResponseSet(responses,indices=responses.keys())
+		test = Test(subjects[case_id],response_set,test_date)
+		tests.append(test)    
+
+	return subjects,tests
 
 def correct_matrix(tests, kind=None):
-    correct = {}
-    for test in tests:
-        if (kind is None) or (test.subject.label == kind):
-            correct[test.subject.case_id]= [int(test.response_set.responses[i].correct) \
-                                    for i in range(1,41)]
-            print(test.subject.case_id,test.response_set.responses[35].correct)
-    
-    return np.array(correct.values())
+	correct = {}
+	for test in tests:
+		if (kind is None) or (test.subject.label == kind):
+			correct[test.subject.case_id]= [int(test.response_set.responses[i].correct) \
+									for i in range(1,41)]
+			print(test.subject.case_id,test.response_set.responses[35].correct)
+	
+	return np.array(correct.values())
 
 def correct_corrs(tests, kind=None):
-    matrix = correct_matrix(tests, kind=kind)
-    for test in tests:
-        if (test.subject.label is None) or (test.subject.label == kind):
-            correct[test.subject]= [int(test.response_set.responses[i].correct) \
-                                    for i in range(1,41)]
+	matrix = correct_matrix(tests, kind=kind)
+	for test in tests:
+		if (test.subject.label is None) or (test.subject.label == kind):
+			correct[test.subject]= [int(test.response_set.responses[i].correct) \
+									for i in range(1,41)]
 
-    corrs = np.corrcoef(matrix.transpose())
-    
-    plt.figure()
-    plt.pcolor(np.arange(0.5,41.5,1),np.arange(0.5,41.5,1),corrs,cmap='RdBu_r',vmin=-1,vmax=1)
-    plt.colorbar()
-    plt.xlim(0.5,40.5)
-    plt.ylim(0.5,40.5)
+	corrs = np.corrcoef(matrix.transpose())
+	
+	plt.figure()
+	plt.pcolor(np.arange(0.5,41.5,1),np.arange(0.5,41.5,1),corrs,cmap='RdBu_r',vmin=-1,vmax=1)
+	plt.colorbar()
+	plt.xlim(0.5,40.5)
+	plt.ylim(0.5,40.5)
 
-    return corrs
+	return corrs
 
 def factor_analysis(tests):
-    from sklearn.decomposition import FactorAnalysis
-    from sklearn.cross_validation import cross_val_score
-    
-    matrix = correct_matrix(tests,kind='ctrl')
-    print(matrix.shape)
-    # matrix must have a number of rows divisible by 3.  
-    # if it does not, eliminate some rows, or pass cv=a to cross_val_score,
-    # where 'a' is a number by which the number of rows is divisible.  
-    fa = FactorAnalysis()
-    fa_scores = []
-    n_components = np.arange(1,41)
-    for n in n_components:
-        fa.n_components = n
-        fa_scores.append(np.mean(cross_val_score(fa, matrix)))
+	from sklearn.decomposition import FactorAnalysis
+	from sklearn.cross_validation import cross_val_score
+	
+	matrix = correct_matrix(tests,kind='ctrl')
+	print(matrix.shape)
+	# matrix must have a number of rows divisible by 3.  
+	# if it does not, eliminate some rows, or pass cv=a to cross_val_score,
+	# where 'a' is a number by which the number of rows is divisible.  
+	fa = FactorAnalysis()
+	fa_scores = []
+	n_components = np.arange(1,41)
+	for n in n_components:
+		fa.n_components = n
+		fa_scores.append(np.mean(cross_val_score(fa, matrix)))
 
-    plt.plot(n_components,fa_scores)
-    
-    return n_components,fa_scores
-    
+	plt.plot(n_components,fa_scores)
+	
+	return n_components,fa_scores
+	
--- scratch.py
+++ scratch.py
@@ -1,8 +1,18 @@
-# Assumes that model5.py has been run
-
 import numpy as np
+import pandas
+from sklearn.naive_bayes import MultinomialNB, BernoulliNB
+from sklearn.cross_validation import cross_val_score,LeaveOneOut,ShuffleSplit
+from sklearn.metrics import roc_curve, auc, roc_auc_score
+from sklearn.preprocessing import scale
+from sklearn.ensemble import RandomForestRegressor,RandomForestClassifier
+from sklearn.multiclass import OneVsRestClassifier,OneVsOneClassifier,OutputCodeClassifier
+from sklearn.preprocessing import MultiLabelBinarizer,Imputer
+from fancyimpute import BiScaler, KNN, NuclearNormMinimization, SoftImpute
+import seaborn as sns
+
 import bbdp
 from upsit import plt
+
 
 def ROC(n_ctrl,means):
     n_subjects = means['r_subject'].shape[0]
@@ -10,10 +20,10 @@ def ROC(n_ctrl,means):
     for i in range(n_subjects):
         pass
 
-def response_matrix(options=['responses'], exclude_subjects={}):
-    ctrl = {}
-    pd = {}
-    subjects,tests = bbdp.load()
+
+def get_response_matrix(kind, options=['responses'], exclude_subjects={}):
+    responses = {}
+    subjects,tests = bbdp.load(kind)
     exclude_subject_ids = []
     for key,value in exclude_subjects.items():
         exclude_subject_ids += [subject_id for subject_id,subject in subjects.items()\
@@ -21,16 +31,10 @@ def response_matrix(options=['responses'], exclude_subjects={}):
     subjects = {subject_id:subject for subject_id,subject in subjects.items()\
                 if subject_id not in exclude_subject_ids}
     tests = [test for test in tests if test.subject.case_id not in exclude_subject_ids]
-    scores = {}
     question_nums = range(1,41)
     possible_responses = [0,1,2,3]
     for test in tests:
-        if test.subject.label == 'ctrl':
-            ctrl[test.subject] = []
-            x = ctrl[test.subject]
-        if test.subject.label == 'pd':
-            pd[test.subject] = []
-            x = pd[test.subject]
+        x = []
         for q in question_nums:
             if 'responses' in options:
                 response = []
@@ -46,20 +50,303 @@ def response_matrix(options=['responses'], exclude_subjects={}):
                 x += [int(test.response_set.responses[q].correct)]            
         if 'total_correct' in options:
             x += [sum([int(test.response_set.responses[q].correct) for q in question_nums])]
+        if 'fraction_correct' in options:
+            x += [sum([int(test.response_set.responses[q].correct) for q in question_nums])/40.0]
         if 'total_responded' in options:
             x += [sum([int(test.response_set.responses[q].choice_num is not None) for q in question_nums])]
         if 'gender' in options:
             x += [test.subject.gender]
+        if 'dementia' in options:
+            x += [test.subject.dementia]
         if 'expired_age' in options:
             x += [test.subject.expired_age]
-     
-    correct = np.array(list(ctrl.values()) + list(pd.values()),dtype='int')
-    if 'num_each_type' in options:
-        n = correct.shape[1]
-        for i in range(4):
-            correct = np.hstack((correct,correct[:,i:n:4].sum(axis=1).reshape(-1,1)))
+        responses[test.subject] = x
 
-    return correct,len(ctrl)
+    ctrl = []
+    for key in sorted(responses.keys(),key=lambda x:x.case_id):
+        ctrl += [subjects[key.case_id].label == 'ctrl']
+    
+    responses = np.array(list(responses[key] for key in \
+                       sorted(responses.keys(),key=lambda x:x.case_id)),dtype='float')
+    
+    
+    if 'num_each_type' in options:
+        n = responses.shape[1]
+        for i in range(4):
+            responses = np.hstack((responses,responses[:,i:n:4].sum(axis=1).reshape(-1,1)))
+
+    return responses,ctrl
+
+
+def get_labels(kind, exclude_subjects={}):
+    labels = {}
+    subjects,tests = bbdp.load(kind)
+    exclude_subject_ids = []
+    for key,value in exclude_subjects.items():
+        exclude_subject_ids += [subject_id for subject_id,subject in subjects.items()\
+                                if subject.__dict__[key]==value]
+    subjects = {subject_id:subject for subject_id,subject in subjects.items()\
+                if subject_id not in exclude_subject_ids}
+    tests = [test for test in tests if test.subject.case_id not in exclude_subject_ids]
+    n_ctrl = 0
+    for test in tests:
+        x = []
+        if test.subject.label == 'ctrl':
+            n_ctrl += 1
+        if kind == 'dugger':
+            x += [test.subject.label == 'pd']
+        #x += [test.subject.expired_age-90]
+        #x += [test.subject.gender]
+        #if hasattr(test.subject,'dementia'):
+        #    x += [test.subject.dementia]
+        #x += [test.subject.stint]
+        if hasattr(test.subject,'other'):
+            x += test.subject.other
+        x = [float(_) for _ in x]
+        labels[test.subject] = x
+    
+    labels = [labels[key] for key in \
+                       sorted(labels.keys(),key=lambda x:x.case_id)]
+    return np.array(labels)
+
+
+def summarize(X_responses,ctrl):
+    print("The loaded matrix has shape (%d x %d), and there are %d controls" % (X_responses.shape[0],
+                                                                            X_responses.shape[1],
+                                                                            np.sum(ctrl)))
+
+
+def plot_total_correct_cumul(X_total_correct,ctrl):
+    X_ctrl = X_total_correct[ctrl == True,0]
+    X_pd = X_total_correct[ctrl == False,0]
+    plt.plot(sorted(X_ctrl),np.linspace(0,1,len(X_ctrl)),'k',label='Control')
+    plt.plot(sorted(X_pd),np.linspace(0,1,len(X_pd)),'r',label='PD')
+    plt.xlabel('Total Correct')
+    plt.ylabel('Cumulative Probability')
+    plt.legend(loc=2)
+
+
+def cross_validate(mnb,X,Y,loo,kind):
+    mean = cross_val_score(mnb,X,Y,cv=loo).mean()
+    print("Cross-validation accuracy for %s is %.3f" % (kind,mean))
+
+
+def get_p_parks(mnb,loo,X,Y):
+    p_parks = []
+    for i, (train, test) in enumerate(loo):
+        mnb.fit(X[train], Y[train])
+        p_parks.append(mnb.predict_proba(X[test])[:,1][0])
+    return p_parks
+
+
+def get_roc_curve(Y,p):
+    fpr, tpr, thresholds = roc_curve(Y, p)
+    roc_auc = auc(fpr, tpr)
+    return fpr,tpr,roc_auc
+
+
+def plot_roc_curve(Y,p_parks_tc,p_parks_r):
+    fpr_tc,tpr_tc,roc_auc_tc = get_roc_curve(Y,p_parks_tc)
+    fpr_r_mnb,tpr_r_mnb,roc_auc_r_mnb = get_roc_curve(Y,p_parks_r)
+    plt.plot(fpr_tc, tpr_tc, lw=2, color='gray', label='AUC using Total Correct = %0.2f' % (roc_auc_tc))
+    #plot(fpr_r_bnb, tpr_r_bnb, lw=2, color='r', label='Responses area = %0.2f' % (roc_auc_r_bnb))
+    plt.plot(fpr_r_mnb, tpr_r_mnb, lw=2, color='g', label='AUC using individual responses = %0.2f' % (roc_auc_r_mnb))
+    plt.xlabel('False Positive Rate', fontsize='large', fontweight='bold')
+    plt.ylabel('True Positive Rate', fontsize='large', fontweight='bold')
+    plt.title('ROC curves', fontsize='large', fontweight='bold')
+    plt.xticks(fontsize='large', fontweight='bold')
+    plt.yticks(fontsize='large', fontweight='bold')
+    plt.legend(loc="lower right")
+
+
+def plot_roc_curves(Y,p,ax=None):
+    if ax is None:
+        fig,ax = plt.subplots(1,1)
+    colors = {'basic':'gray','total':'pink','all':'red'}
+    for key in p:
+        fpr,tpr,auc = get_roc_curve(Y[key],p[key])
+        color = 'red' if key not in colors else colors[key]
+        ax.plot(fpr, tpr, lw=2, color=color, label='AUC using\n%s = %0.2f' % (key,auc))
+    ax.set_xlim(-0.01,1.01)
+    ax.set_ylim(-0.01,1.01)
+    ax.set_xlabel('False Positive Rate')#, fontsize='large', fontweight='bold')
+    ax.set_ylabel('True Positive Rate')#, fontsize='large', fontweight='bold')
+    ax.set_title('ROC curves')#, fontsize='large', fontweight='bold')
+    # Set the tick labels font
+    for label in (ax.get_xticklabels() + ax.get_yticklabels()):
+        pass
+        #label.set_fontsize('large')
+        #label.set_fontweight('bold')
+    ax.legend(loc="lower right")
+
+
+def roc_data(X,Y,clf,n_iter=50,test_size=0.1):
+    if n_iter is None and test_size is None:
+        cv = LeaveOneOut(Y.shape[0])
+    else:
+        cv = ShuffleSplit(Y.shape[0],n_iter=n_iter,test_size=test_size)
+    n_labels = Y.shape[1]
+    Y_cv = {i:[] for i in range(n_labels)}
+    p = {i:[] for i in range(n_labels)}
+    p_1 = {i:[] for i in range(n_labels)}
+    p_0 = {i:[] for i in range(n_labels)}
+    for train, test in cv:
+        clf.fit(X[train,:], Y[train,:])
+        Y_predicted = clf.predict_proba(X[test,:])
+        for i in range(Y.shape[1]):
+            if type(Y_predicted) is list:
+                p_ = 1 - Y_predicted[i][:,0]
+            else:
+                p_ = Y_predicted[:,i]
+            Y_cv[i] += list(Y[test,i])
+            p[i] += list(p_)
+            p_1[i] += list(p_[np.where(Y[test,i]==1)[0]])
+            p_0[i] += list(p_[np.where(Y[test,i]==0)[0]])
+    return Y_cv, p, p_1, p_0
+
+
+def violin_roc(data):
+    plt.figure(figsize=(15, 10))
+    sns.set_context("notebook", font_scale=2.5, 
+                    rc={"lines.linewidth": 1.5, 'legend.fontsize': 20})
+    sns.violinplot(x='Predicted Probability', y='Diagnosis', hue='Outcome', 
+                   data=data, split=True, inner="quart", 
+                   palette={'--': "y", '+': "b"}, orient='h', width=1.0, 
+                   scale='area',#count
+                   order=['VaD','Tauopathy NOS','AG','DLB','LB','ILBD',
+                          'PD','AD','Parkinsonism NOS','PSP'])
+    leg = plt.gca().get_legend()
+    ltext  = leg.get_texts()  # all the text.Text instance in the legend
+    plt.setp(ltext, fontsize=24)    # the legend text fontsize
+    plt.xlim(0,1)
+    sns.despine(left=True)
+
+
+def plot_roc_curves_with_ps(Y,Y_cv,Xs,p0,p1,p,pathological):
+    fig,ax = plt.subplots(Y.shape[1],4)
+    sns.set_context("notebook", font_scale=1, rc={"lines.linewidth": 1.5, 'legend.fontsize': 12})
+    fig.set_size_inches(15,30)
+    for i in range(Y.shape[1]):
+        for j,key in enumerate(['basic','total','all']):
+            X = Xs[key]
+            if len(p1[key][i]) and len(p0[key][i]):
+                ax[i,j].hist(p1[key][i],bins=1000,range=(0,1),color='r',normed=True,cumulative=True,histtype='step')
+                ax[i,j].hist(p0[key][i],bins=1000,range=(0,1),color='k',normed=True,cumulative=True,histtype='step')
+                ax[i,j].set_title(pathological.columns.values[i])
+            if i==ax.shape[0]-1:
+                ax[i,j].set_xlabel('Predicted p(pathology)')
+            if j==0:
+                ax[i,j].set_ylabel('Cumulative fraction')
+            ax[i,j].set_xlim(0,1)
+            ax[i,j].set_ylim(0,1)
+        plot_roc_curves({key:Y_cv[key][i] for key in Y_cv},
+                        {key:p[key][i] for key in p},
+                        ax=ax[i,3])
+
+    fig.tight_layout()
+
+
+def plot_just_rocs(Y_clean,ps,ys,imps):
+    fig,axes = plt.subplots(4,4,sharex=True,sharey=True,figsize=(10,10))
+    y = {}
+    p = {}
+    for i in range(len(list(Y_clean))):
+        ax = axes.flat[i]
+        for imp in imps:
+            pimp = ps[imp][i,:,:].ravel() # Unravel all predictions of test data over all cv splits.  
+            yimp = ys[imp][i,:,:].ravel() # Unravel all test data ground truth over all cv splits.  
+            pimp = pimp[np.isnan(yimp)==0] # Remove NaNs (no ground truth)
+            yimp = yimp[np.isnan(yimp)==0] # Remove NaNs (no ground truth)
+            p[imp] = pimp.copy()
+            y[imp] = yimp.copy()
+        plot_roc_curves(y,p,ax=ax) # Plot on the given axes.  
+        ax.set_title(list(Y_clean)[i]) # Set the title.  
+    for i in range(i+1,len(axes.flat)):
+        ax = axes.flat[i]
+        ax.set_axis_off()
+    plt.tight_layout()
+
+
+def report(rs,Y_clean,imps):
+    n_props = list(rs.values())[0].shape[0]
+    n_splits = list(rs.values())[0].shape[1]
+    for prop in range(n_props):
+        prop_name = list(Y_clean)[prop]
+        for imp in imps:
+            vals = rs[imp][prop,:]
+            print('%s,%s: %.3f +/- %.3f' % (imp,prop_name,vals.mean(),vals.std()/np.sqrt(n_splits)))
+
+
+def build_p_frame(p0,p1,pathological,guide):
+    exclude = ['demunp','ftdpdefined','hs','cbdp'] # Exclude these labels from further analysis.  
+    ps = [] # List of information for data frame.  
+    for y,p_ in [(0,p0),(1,p1)]: # Iterate over no/yes and probabilities of no/yes.  
+        for i in p_['all']: # Iterate over labels (AD, PD, etc.)
+            label = pathological.columns.values[i] # Name of label.  
+            if label in exclude: # Exclude if in the exclude list.  
+                continue
+            label = guide.query("Name=='%s'" % label)['Label'].values[0].replace('FD ','') # Fix label.  
+            for value in p_['all'][i]: # Iterate over subjects.  
+                ps.append((label,value,'+' if y else '--')) # Fill the list.  
+    ps = pandas.DataFrame(ps, columns=['Diagnosis','Predicted Probability','Outcome']) # Convert to a data frame.  
+    return ps
+
+
+def fit_models(imps,X,Y,Y_clean,n_estimators=25,n_splits=5):
+    n_obs = X['missing'].shape[0] # Number of observations.  
+    n_features = X['missing'].shape[1] # Number of observations.  
+    n_props = Y['missing'].shape[1] # Number of properties to predict.  
+    shuffle_split = ShuffleSplit(n_obs,n_iter=n_splits,test_size=0.2,random_state=0)
+    rs = {imp:np.ma.zeros((n_props,n_splits)) for imp in imps}
+    ps = {imp:np.zeros((n_props,n_splits,176)) for imp in imps}
+    ys = {imp:np.zeros((n_props,n_splits,176)) for imp in imps}
+    feature_importances = {imp:np.ma.zeros((n_props,n_features,n_splits)) for imp in imps}
+    for prop in range(n_props):
+        print("Fitting model for %s..." % list(Y_clean)[prop])
+        for imp in imps:
+            for i,(train,test) in enumerate(shuffle_split):
+                X_train,X_test = X[imp][train],X[imp][test]
+                Y_train,Y_test = Y[imp][train,prop],Y['missing'][test,prop]
+                rfc = RandomForestClassifier(n_estimators=n_estimators,random_state=0)
+                #if Y_train.shape[1] == 1:
+                #    Y_train = Y_train.ravel()
+                rfc.fit(X_train,Y_train)
+                Y_predict = rfc.predict(X_test)#.reshape(-1,n_props)
+                probs = rfc.predict_proba(X_test)
+                if probs.shape[1]<2 and probs.mean()==1.0:
+                    ps[imp][prop,i,:] = 0.0
+                else:
+                    ps[imp][prop,i,:] = probs[:,1]
+                ys[imp][prop,i,:] = Y_test
+                rs[imp][prop,i] = np.ma.corrcoef(Y_predict,Y_test)[0,1]
+                feature_importances[imp][prop,:,i] = rfc.feature_importances_
+    return rs,feature_importances,ys,ps
+
+
+def imputation(clean,imps=['knn','nmm','softimpute','biscaler']):
+    imputer = Imputer(strategy='median',axis=0)
+    X = {'missing':clean.as_matrix().astype('float')}
+    X['median'] = imputer.fit_transform(X['missing'])
+    
+    if 'knn' in imps:
+        X['knn'] = KNN(k=3).complete(X['missing'])
+    if 'nnm' in imps:
+        X['nnm'] = NuclearNormMinimization().complete(X['missing'])
+    if 'softimpute' in imps:
+        X['softimpute'] = SoftImpute().complete(X['missing'])
+    
+    X['missing'] = np.ma.array(X['missing'],mask=np.isnan(X['missing']))
+    return X
+
+
+def display_importances(X_clean,Y_clean,feature_importances):
+    df_importances = pandas.DataFrame(columns=list(Y_clean))
+    f_importance_means = feature_importances['knn'].mean(axis=2)
+    for i,diagnosis in enumerate(list(Y_clean)):
+        f_d_importance_means = [(feature,f_importance_means[i,j].round(3)) for j,feature in enumerate(list(X_clean))]
+        df_importances[diagnosis] = sorted(f_d_importance_means,key=lambda x:x[1],reverse=True)
+    return df_importances.head(10)
+
 
 def classify(n_ctrl,data,alpha=1.0):
     """Naive bayes prediction of test results."""
@@ -82,6 +369,7 @@ def classify(n_ctrl,data,alpha=1.0):
     print("\nClassification Accuracy: %.3f +/- %.3f" \
           % (scores.mean(),scores.std()/np.sqrt(i)))
     return clf,X,Y
+
 
 def classify2(n_ctrl,data,clf=None):
     from sklearn.neural_network import BernoulliRBM
